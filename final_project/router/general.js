c305530d8c0025cbe35222528d7969037f85fbef
JBrownfield4 <justinbrownfield2000@gmail.com>
2025-02-03 08:48:53 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,18 +23,18 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
   });
 // Check if a user with the given username already exists
-const doesExist = (username) => {
+//const doesExist = (username) => {
     // Filter the users array for any user with the same username
-    let userswithsamename = users.filter((user) => {
-        return user.username === username;
-    });
+  //  let userswithsamename = users.filter((user) => {
+    //    return user.username === username;
+    //});
     // Return true if any user with the same username is found, otherwise false
-    if (userswithsamename.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+    //if (userswithsamename.length > 0) {
+      //  return true;
+    //} else {
+      //  return false;
+    //}
+//}
 // Check if the user with the given username and password exists
 const authenticatedUser = (username, password) => {
     // Filter the users array for any user with the same username and password
