765c62b4ef725caa29e53a93ea1b8fa9ac58d61d
JBrownfield4 <justinbrownfield2000@gmail.com>
2025-02-03 08:50:37 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,31 +23,31 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
   });
 // Check if a user with the given username already exists
-//const doesExist = (username) => {
+const doesExist = (username) => {
     // Filter the users array for any user with the same username
-  //  let userswithsamename = users.filter((user) => {
-    //    return user.username === username;
-    //});
-    // Return true if any user with the same username is found, otherwise false
-    //if (userswithsamename.length > 0) {
-      //  return true;
-    //} else {
-      //  return false;
-    //}
-//}
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-    // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
+     let userswithsamename = users.filter((user) => {
+       return user.username === username;
     });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
         return true;
     } else {
         return false;
     }
 }
+// Check if the user with the given username and password exists
+//const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    //let validusers = users.filter((user) => {
+        //return (user.username === username && user.password === password);
+    //});
+    // Return true if any valid user is found, otherwise false
+    //if (validusers.length > 0) {
+        //return true;
+    //} else {
+        //return false;
+    //}
+//}
 
 
   
